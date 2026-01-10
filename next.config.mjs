a90d1d840c5488bc73e0b5f24fed6fc8aa5ac642
Mathi Sankar M R <mathisankar707@gmail.com>
2026-01-10 11:25:00 +0530

    Update configuration

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+export default nextConfig;
